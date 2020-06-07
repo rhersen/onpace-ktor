2fb86409d883ce42a56078450a186b8ec7d9b690
Rudolf Hersén <rudolf.hersen@schibsted.com>
2020-06-07 22:29:56 +0200

    fix indentation

--- src/Application.kt
+++ src/Application.kt
@@ -32,102 +32,115 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(@Suppress("UNUSED_PARAMETER") testing: Boolean = false) {
-    install(ContentNegotiation) {
-        jackson {
-            enable(SerializationFeature.INDENT_OUTPUT)
-        }
+  install(ContentNegotiation) {
+    jackson {
+      enable(SerializationFeature.INDENT_OUTPUT)
     }
+  }
 
-    val client = HttpClient(Apache) {
-        install(JsonFeature) {
-            serializer = JacksonSerializer()
-        }
+  val client = HttpClient(Apache) {
+    install(JsonFeature) {
+      serializer = JacksonSerializer()
     }
+  }
 
     val redirectUri = URLEncoder.encode(
         "https://onpace-ktor.herokuapp.com/logged-in", "UTF-8"
     )
 
-    routing {
-        get("/") {
-            call.respondHtml {
-                body {
-                    a(href = "login") { +"login" }
-                }
-            }
+  routing {
+    get("/") {
+      call.respondHtml {
+        body {
+          a(href = "login") { +"login" }
         }
-
-        get("/login") {
-            call.respondRedirect(
-                "https://www.strava.com/oauth/authorize?${arrayOf(
-                    "client_id=45920",
-                    "response_type=code",
-                    "scope=read",
-                    "redirect_uri=$redirectUri"
-                ).joinToString("&")}"
-            )
-        }
-
-        get("/logged-in") {
-            try {
-                val code: String = call.request.queryParameters["code"].toString()
-                val clientId = System.getenv("CLIENT_ID")
-                val clientSecret = System.getenv("CLIENT_SECRET")
-
-                when {
-                    clientId == null -> {
-                        call.response.status(HttpStatusCode.InternalServerError)
-                        call.respondHtml(HttpStatusCode.BadRequest) { body { div { +"CLIENT_ID missing" } } }
-                    }
-                    clientSecret == null -> {
-                        call.response.status(HttpStatusCode.InternalServerError)
-                        call.respondHtml(HttpStatusCode.BadRequest) { body { div { +"CLIENT_SECRET missing" } } }
-                    }
-                    else -> {
-                        val authentication: Authentication = client.submitForm(
-                            "https://www.strava.com/api/v3/oauth/token",
-                            Parameters.build {
-                                append("code", code)
-                                append("client_id", clientId)
-                                append("client_secret", clientSecret)
-                                append("grant_type", "authorization_code")
-                            }
-                        )
-
-                        val activityStats: ActivityStats =
-                            client.get("https://www.strava.com/api/v3/athletes/${authentication.athlete.id}/stats") {
-                                header("Authorization", "Bearer ${authentication.access_token}")
-                            }
-
-                        val distance: Double = activityStats.ytd_run_totals.distance
-                        val target: Double = 15e5 * LocalDate.now().dayOfYear / 366.0
-
-                        call.respondHtml {
-                            body {
-                                div {
-                                    h4 { +"löpning" }
-                                    div {
-                                        span { +String.format("%.1f", distance * 1e-3) }
-                                        +"/"
-                                        span { +"${String.format("%.1f", target * 1e-3)} km" }
-                                    }
-                                    div {
-                                        +(
-                                                if (target < distance)
-                                                    "${String.format("%.0f", distance - target)} meter före"
-                                                else
-                                                    "${String.format("%.0f", target - distance)} meter efter"
-                                                )
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            } catch (e: ClientRequestException) {
-                val httpResponse = e.response
-                call.respondText(httpResponse.readText(), contentType = ContentType.Text.Plain)
-            }
-        }
+      }
     }
+
+    get("/login") {
+      call.respondRedirect(
+        "https://www.strava.com/oauth/authorize?${arrayOf(
+          "client_id=45920",
+          "response_type=code",
+          "scope=read",
+          "redirect_uri=$redirectUri"
+        ).joinToString("&")}"
+      )
+    }
+
+    get("/logged-in") {
+      try {
+        val code: String = call.request.queryParameters["code"].toString()
+        val clientId = System.getenv("CLIENT_ID")
+        val clientSecret = System.getenv("CLIENT_SECRET")
+
+        when {
+          clientId == null -> {
+            call.response.status(HttpStatusCode.InternalServerError)
+            call.respondHtml(HttpStatusCode.BadRequest) {
+              body {
+                div {
+                  +"CLIENT_ID missing"
+                }
+              }
+            }
+          }
+          clientSecret == null -> {
+            call.response.status(HttpStatusCode.InternalServerError)
+            call.respondHtml(HttpStatusCode.BadRequest) {
+              body {
+                div {
+                  +"CLIENT_SECRET missing"
+                }
+              }
+            }
+          }
+          else -> {
+            val authentication: Authentication = client.submitForm(
+              "https://www.strava.com/api/v3/oauth/token",
+              Parameters.build {
+                append("code", code)
+                append("client_id", clientId)
+                append("client_secret", clientSecret)
+                append("grant_type", "authorization_code")
+              })
+
+            val activityStats: ActivityStats =
+              client.get("https://www.strava.com/api/v3/athletes/${authentication.athlete.id}/stats") {
+                header("Authorization", "Bearer ${authentication.access_token}")
+              }
+
+            val distance: Double = activityStats.ytd_run_totals.distance
+            val target: Double = 15e5 * LocalDate.now().dayOfYear / 366.0
+
+            call.respondHtml {
+              body {
+                div {
+                  h4 { +"löpning" }
+                  div {
+                    span { +String.format("%.1f", distance * 1e-3) }
+                    +"/"
+                    span { +"${String.format("%.1f", target * 1e-3)} km" }
+                  }
+                  div {
+                    +(if (target < distance) "${String.format(
+                      "%.0f", distance - target
+                    )} meter före"
+                    else "${String.format(
+                      "%.0f", target - distance
+                    )} meter efter")
+                  }
+                }
+              }
+            }
+          }
+        }
+      } catch (e: ClientRequestException) {
+        val httpResponse = e.response
+        call.respondText(
+          httpResponse.readText(), contentType = ContentType.Text.Plain
+        )
+      }
+    }
+  }
 }
